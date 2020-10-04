c02f77b79a1e0d66ee34302df97a9d2ba0b2a735
Jessica Stokes <hello@jessicastokes.net>
2020-10-03 23:06:46 -0700

    Init ps2hdd

---
+++ ps2hdd/src/lib.rs
@@ -0,0 +1,11 @@
+//! A Rust wrapper for `libps2hdd`, the library version of
+//! [`pfsshell`](https://github.com/ps2homebrew/pfsshell), providing utilities
+//! for reading and writing PlayStation 2 format hard disks and disk images.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
