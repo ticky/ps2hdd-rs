3c26d10f9f510ba096b0b117d01e4284a3658c9d
Jessica Stokes <hello@jessicastokes.net>
2020-10-06 21:18:32 -0700

    Refactor some more of the tests, test re-mounting disk images
    Also moves some stuff to utility functions, which are probably a good imitation of what the future nice-wrapper will be like.

--- ps2hdd-sys/src/lib.rs
+++ ps2hdd-sys/src/lib.rs
@@ -27,6 +27,13 @@ mod tests {
     // now I know for sure. lmao.
     use serial_test::serial;
 
+    /// Utility function which sets the `atad_device_path` variable
+    /// to the content of a given Rust str value.
+    ///
+    /// Handles conversion to a CString, and null-termination.
+    ///
+    /// Needed because the underlying type of bytes of a CString and
+    /// those of a c_char are, sadly, different (u8 vs i8, damn it C)
     fn util_set_atad_device_path(path: &str) {
         let name_slice = std::ffi::CString::new(path)
             .expect("could not convert atad_device_path slice to C String");
@@ -34,75 +41,105 @@ mod tests {
         let length = name_slice.as_bytes().len();
 
         if length > 255 {
-            panic!("util_set_atad_device_path: string length {} is too long to be null-terminated", length)
+            panic!(
+                "util_set_atad_device_path: string length {} is too long to be null-terminated",
+                length
+            )
         }
 
         name_slice
             .as_bytes()
             .iter()
             .enumerate()
-            .for_each(|(index, byte)| {
-                unsafe {
-                    atad_device_path[index] = *byte as i8;
-                    if index == length {
-                        atad_device_path[index + 1] = 0x00;
-                    }
+            .for_each(|(index, byte)| unsafe {
+                atad_device_path[index] = *byte as i8;
+                if index == length - 1 {
+                    atad_device_path[index + 1] = 0x00;
                 }
             });
 
         unsafe {
-            let demo_file_path = std::ffi::CStr::from_ptr(atad_device_path.as_ptr())
+            let after_path = std::ffi::CStr::from_ptr(atad_device_path.as_ptr())
                 .to_str()
                 .expect("could not convert the updated device path to a String");
             assert_eq!(
-                demo_file_path, path,
+                after_path, path,
                 "util_set_atad_device_path: updating the variable didn't work, weird!"
             );
         }
     }
 
-    #[test]
-    #[serial(atad_device_path)]
-    fn multiple_init_calls() {
-        unsafe {
-            let demo_file_path = std::ffi::CStr::from_ptr(atad_device_path.as_ptr())
+    /// Utility function which, when passed a CString path,
+    /// reads the list of the items within that directory.
+    fn util_iomanx_dopen_dread(path: &std::ffi::CString) -> Vec<iox_dirent_t> {
+        let mut temp_dirent: iox_dirent_t = unsafe { std::mem::zeroed() };
+        let mut dirents = Vec::new();
+
+        let device_handle = unsafe { iomanx_dopen(path.as_ptr()) };
+
+        if device_handle < 0 {
+            let err = unsafe { std::ffi::CStr::from_ptr(libc::strerror(-device_handle)) }
                 .to_str()
-                .expect("could not convert the default device path to a String");
-            assert_eq!(
-                demo_file_path, "hdd.img",
-                "not using the default hdd.img file (are you doing multiple threads?)"
+                .expect("util_iomanx_dopen_dread: could not convert error message a String");
+            panic!(
+                "util_iomanx_dopen_dread: dopen failed: {}, {}",
+                device_handle, err
             );
-
-            let demo_1_path = "hdd0.img";
-            util_set_atad_device_path(demo_1_path);
-
-            // TODO: I was going to check that calling `_init_apa`
-            // multiple times works okay, and doesn't end up breaking things
-            // but instead I am going to play some Yakuza 0 for now.
-
-            // Always re-set this after each test
-            util_set_atad_device_path("hdd.img");
         }
+
+        while {
+            let result = unsafe { iomanx_dread(device_handle, &mut temp_dirent) };
+
+            // aaaaAAAAaaa this took me SO long to work out;
+            // - less than zero means there was an error,
+            // - zero means there are no more directory entries
+            // - greater than zero means a file handle number
+            if result < 0 {
+                let name = unsafe { std::ffi::CStr::from_ptr(temp_dirent.name.as_ptr()) }
+                    .to_str()
+                    .expect(
+                        "util_iomanx_dopen_dread: could not convert the partition name to a String",
+                    );
+                println!("util_iomanx_dopen_dread: result: {} {}", result, name);
+            }
+
+            result > 0
+        } {
+            dirents.push(temp_dirent);
+        }
+
+        unsafe {
+            iomanx_close(device_handle);
+        }
+
+        dirents
     }
+
+    // A disk image needs to be at least 6GB in size for APA to work
+    // more is better, and 20GB is the "normal" minimum, but this is
+    // just enough  to fit any of the minimum-size 128MB partitions in
+    static DEMO_FILE_SIZE: u64 = 6 * 1024 * 1024 * 1024;
+    static DEFAULT_FILE_PATH: &str = "hdd.img";
 
     #[test]
     #[serial(atad_device_path)]
     fn format_list_partitions_and_write() {
         unsafe {
+            // NOTE: Because the str would be pointing at the same data as the
+            // C pointer, later writes would cause problems if we don't turn
+            // this into a real String.
             let demo_file_path = std::ffi::CStr::from_ptr(atad_device_path.as_ptr())
                 .to_str()
-                .expect("could not convert the default device path to a String");
+                .expect("could not convert the default device path to a String")
+                .to_owned();
             assert_eq!(
-                demo_file_path, "hdd.img",
+                demo_file_path, DEFAULT_FILE_PATH,
                 "not using the default hdd.img file (are you doing multiple threads?)"
             );
 
-            // Create hdd.img, and make it 6GB of zeroes
-            // NOTE: This is way bigger than I'd hoped, but it's the minimum
-            // required to fit any of the minimum-size partitions in.
-            File::create(demo_file_path)
+            File::create(&demo_file_path)
                 .expect("couldn't create demo file")
-                .set_len(6 * 1024 * 1024 * 1024)
+                .set_len(DEMO_FILE_SIZE)
                 .expect("couldn't make demo file the right size");
 
             // NOTE: The `_init_*` methods initialise each driver, adding it to
@@ -138,11 +175,11 @@ mod tests {
             //     "_init_hdlfs failed"
             // );
 
-            let format_path = std::ffi::CString::new("hdd0:").expect("couldn't convert string");
+            let hdd0_path = std::ffi::CString::new("hdd0:").expect("couldn't convert string");
 
             assert_eq!(
                 iomanx_format(
-                    format_path.as_ptr(),
+                    hdd0_path.as_ptr(),
                     std::ptr::null_mut(),
                     std::ptr::null_mut(),
                     0
@@ -170,9 +207,33 @@ mod tests {
 
             assert_eq!(iomanx_close(fd), 0, "iomanx_close failed");
 
+            // Check the partition listing
+
+            let partitions: Vec<_> = util_iomanx_dopen_dread(&hdd0_path)
+                .iter()
+                .map(|dirent| {
+                    std::ffi::CStr::from_ptr(dirent.name.as_ptr())
+                        .to_str()
+                        .expect("could not convert partition entry name")
+                })
+                .collect();
+
+            assert_eq!(
+                partitions,
+                vec![
+                    "__mbr",
+                    "__net",
+                    "__system",
+                    "__sysconf",
+                    "__common",
+                    "TESTPART"
+                ],
+                "unexpected partition list"
+            );
+
             let testpart_path = std::ffi::CString::new("hdd0:TESTPART")
                 .expect("couldn't convert mount path string");
-            let pfs_path =
+            let pfs0_root_path =
                 std::ffi::CString::new("pfs0:").expect("couldn't convert pfs path string");
 
             // Formatting the TESTPART partition
@@ -182,7 +243,7 @@ mod tests {
             let mut format_arg: [i32; 3] = [PFS_ZONE_SIZE, 0x2d66, PFS_FRAGMENT];
 
             let format_result = iomanx_format(
-                pfs_path.as_ptr(),
+                pfs0_root_path.as_ptr(),
                 testpart_path.as_ptr(),
                 format_arg.as_mut_ptr() as *mut core::ffi::c_void,
                 std::mem::size_of::<[i32; 3]>() as u64,
@@ -196,7 +257,7 @@ mod tests {
             }
 
             let mount_result = iomanx_mount(
-                pfs_path.as_ptr(),
+                pfs0_root_path.as_ptr(),
                 testpart_path.as_ptr(),
                 0,
                 std::ptr::null_mut(),
@@ -212,10 +273,10 @@ mod tests {
 
             // Making a test directory
 
-            let mkdir_path = std::ffi::CString::new("pfs0:/testdir")
+            let pfs0_testdir_path = std::ffi::CString::new("pfs0:/testdir")
                 .expect("couldn't convert mkdir path string");
 
-            let mkdir_result = iomanx_mkdir(mkdir_path.as_ptr(), 0o777);
+            let mkdir_result = iomanx_mkdir(pfs0_testdir_path.as_ptr(), 0o777);
 
             if mkdir_result < 0 {
                 let err = std::ffi::CStr::from_ptr(libc::strerror(-mkdir_result))
@@ -226,81 +287,101 @@ mod tests {
 
             // Opening the root directory so we can list it
 
-            let ls_path =
+            let pfs0_ls_path =
                 std::ffi::CString::new("pfs0:/").expect("couldn't convert ls path string");
-
-            let root_dh = iomanx_dopen(ls_path.as_ptr());
-
-            if root_dh < 0 {
-                let err = std::ffi::CStr::from_ptr(libc::strerror(-root_dh))
-                    .to_str()
-                    .expect("could not convert error message a String");
-                panic!("iomanx_dopen failed: {}, {}", root_dh, err);
-            }
 
             // Grab all of the directory entries and put them in a vec
 
-            let mut dirent: iox_dirent_t = std::mem::zeroed();
-            let mut dirents = Vec::new();
-            while {
-                let result = iomanx_dread(root_dh, &mut dirent);
-
-                // aaaaAAAAaaa this took me SO long to work out;
-                // - less than zero means there was an error,
-                // - zero means there are no more directory entries
-                // - greater than zero means a file handle number
-                if result < 0 {
-                    let name = std::ffi::CStr::from_ptr(dirent.name.as_ptr())
-                        .to_str()
-                        .expect(
-                            "looping iomanx_dread, could not convert the file name to a String",
-                        );
-                    println!("looping iomanx_dread, result: {} {}", result, name);
-                }
-
-                result > 0
-            } {
-                dirents.push(dirent);
-            }
+            let pfs0_dirents = util_iomanx_dopen_dread(&pfs0_ls_path);
 
             // Check the directory listing
 
-            assert_eq!(dirents.len(), 3, "unexpected number of directory entries");
+            let pfs0_dirnames: Vec<_> = pfs0_dirents
+                .iter()
+                .map(|dirent| {
+                    std::ffi::CStr::from_ptr(dirent.name.as_ptr())
+                        .to_str()
+                        .expect("could not convert directory entry name")
+                })
+                .collect();
 
             assert_eq!(
-                std::ffi::CStr::from_ptr(dirents[0].name.as_ptr())
-                    .to_str()
-                    .expect("could not convert directory entry name 0"),
-                "."
+                pfs0_dirnames,
+                vec![".", "..", "testdir"],
+                "unexpected directory list"
             );
+
             assert_eq!(
-                std::ffi::CStr::from_ptr(dirents[1].name.as_ptr())
-                    .to_str()
-                    .expect("could not convert directory entry name 1"),
-                ".."
-            );
-            assert_eq!(
-                std::ffi::CStr::from_ptr(dirents[2].name.as_ptr())
-                    .to_str()
-                    .expect("could not convert directory entry name 2"),
-                "testdir"
-            );
-            assert_eq!(
-                dirents[2].stat.mode & FIO_S_IFMT,
+                pfs0_dirents[2].stat.mode & FIO_S_IFMT,
                 FIO_S_IFDIR,
                 "expected item at index 2 to be a directory"
             );
 
-            iomanx_close(root_dh);
-
             // This cleans up ATAD's real file pointer, but nothing else;
-            // technically between tests the rest of the infrastructure is
-            // still prepared to run. Alas.
+            // technically between these phases the rest of the infrastructure
+            // is still prepared to run. Alas.
             atad_close();
 
-            assert_eq!(iomanx_umount(pfs_path.as_ptr()), 0, "iomanx_umount failed");
+            assert_eq!(
+                iomanx_umount(pfs0_root_path.as_ptr()),
+                0,
+                "iomanx_umount failed"
+            );
+
+            let demo_1_path = "hdd1.img";
+
+            File::create(demo_1_path)
+                .expect("couldn't create 1st demo file")
+                .set_len(DEMO_FILE_SIZE)
+                .expect("couldn't make 1st demo file the right size");
+
+            util_set_atad_device_path(demo_1_path);
+
+            assert_eq!(
+                _init_apa(0, std::ptr::null_mut()),
+                0,
+                "second _init_apa failed"
+            );
+
+            assert_eq!(
+                iomanx_format(
+                    hdd0_path.as_ptr(),
+                    std::ptr::null_mut(),
+                    std::ptr::null_mut(),
+                    0
+                ),
+                0,
+                "second iomanx_format failed"
+            );
+
+            // Check the partition listing
+
+            let partitions: Vec<_> = util_iomanx_dopen_dread(&hdd0_path)
+                .iter()
+                .map(|dirent| {
+                    std::ffi::CStr::from_ptr(dirent.name.as_ptr())
+                        .to_str()
+                        .expect("could not convert partition entry name")
+                })
+                .collect();
+
+            assert_eq!(
+                partitions,
+                vec![
+                    "__mbr",
+                    "__net",
+                    "__system",
+                    "__sysconf",
+                    "__common"
+                ],
+                "unexpected partition list"
+            );
 
             std::fs::remove_file(demo_file_path).expect("could not delete demo file");
+            std::fs::remove_file(demo_1_path).expect("could not delete demo 1 file");
+
+            // Always re-set this after each test
+            util_set_atad_device_path(DEFAULT_FILE_PATH);
         }
     }
 }
